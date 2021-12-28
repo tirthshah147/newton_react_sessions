74545b39327cc5f6acced0c10d9fa93f14bab06a
Tirth Shah <themadpreneur@Tirths-MacBook-Air.local>
2021-12-28 18:53:55 +0530

    Robo project completed. API integrated

--- roboproject1/src/App.js
+++ roboproject1/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
-import robots from './data';
+// import robots from './data';
 import SearchBox from './SearchBox';
 
 
@@ -14,14 +14,23 @@ import SearchBox from './SearchBox';
 //   );
 // }
 
+//method 1
+
+//method 2
+
+//method 3
+
 class App extends React.Component{
   
   constructor(){
     super();
     this.state = {
-      robots : robots,
+      // robots : robots,
+      robots : [],
       searchField : '',
     }
+
+    console.log('Constructor is running...')
   }
   
 
@@ -30,11 +39,38 @@ class App extends React.Component{
       console.log(this.state.searchField);
   }
 
+  // 54 < 65 < 78
+  // 65 > 45 > 34
+
+  // static getDerivedStateFromProps(){
+  //   console.log("getDerivedStateFromProps is running..")
+  // }
+
+  componentDidMount = () => {
+    // console.log("Component did mount is running...")
+    // await this.setState({robots : robots})
+    console.log("Component Did Mount is running");
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {
+          // console.log(users);
+          this.setState({robots : users})
+        })
+  }
+
 
   render(){
-    const filteredRobots = this.state.robots.filter((robot) => {
-        return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    console.log('Render is running...');
+
+    const {robots, searchField} = this.state;
+
+    const filteredRobots = robots.filter((robot) => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
+
+    if (!robots.length) {    // robots.length === 0  means we want to run
+      return <h1>Loading....</h1>
+    }
      
     return (
       <div className="tc">
